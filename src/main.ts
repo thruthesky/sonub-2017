40d81e653bcd8705ad6a4ea5cddd0ecc85985aec
JaeHo Song <thruthesky@gmail.com>
2017-07-21 16:27:05 +0800

    update for refactoring

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,28 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { hmrBootstrap } from './hmr';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+// platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+// 수정 할 부분: 기존의 bootstrap 문장을 bootstrap() 함수로 만든다.
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+// 아래는 추가해야 할 부분.
+if (environment.hmr) {
+  if (module[ 'hot' ]) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap();
+}
+
