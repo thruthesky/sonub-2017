18487741fd8c06f88bb61228ee2e66fc21526115
JaeHo Song <thruthesky@gmail.com>
2017-08-08 21:12:31 +0800

    update for header layout

---
+++ src/modules/etc/pages/menu/menu.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'menu-page',
+    templateUrl: 'menu.html'
+})
+
+export class MenuPage implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
